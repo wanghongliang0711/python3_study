cfa397989822ca547865f5b603d79606ab2a349e
blake <blake.wang@mic.com.tw>
2021-04-26 15:23:01 +0800

    2020 0619 company openpyxl 两个sheet

---
+++ study_openpyxl/openpyxl_xlsx/TwoSheet.py
@@ -0,0 +1,16 @@
+﻿#-*-coding:utf-8 -*-
+import os
+import openpyxl
+
+path66 = os.getcwd()+'\\write.xlsx'
+data = openpyxl.Workbook() 
+sheet1 = data.active
+sheet1.title = 'Statistical results'
+sheet1['A1'] = 'good'
+
+sheet2 = data.create_sheet('Detailed results')
+sheet2['A1'] = 'good333'
+
+
+
+data.save(path66)
