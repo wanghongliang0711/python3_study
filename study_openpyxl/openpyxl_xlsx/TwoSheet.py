d3508bb5ebb10c968d4652a0ae6224a18946544d
blake <blake.wang@mic.com.tw>
2021-07-16 09:50:00 +0800

    2021 0716 company None 插入excel表格也是空的，看起来和""的效果一样，以后可以用None用初始化要插入到excel的数据

--- study_openpyxl/openpyxl_xlsx/TwoSheet.py
+++ study_openpyxl/openpyxl_xlsx/TwoSheet.py
@@ -8,6 +8,11 @@ sheet1 = data.active
 sheet1.title = 'Statistical results'
 sheet1['A1'] = 'good'
 
+row = [1 ,2, 3, 4, 5]
+row1 = [1 ,"", None, None, 5]
+sheet1.append(row)
+sheet1.append(row1)
+
 sheet2 = data.create_sheet('Detailed results')
 sheet2['A1'] = 'good333'
 
